f3d2741a528c2b4af4dfca81a89ee6cd5cd4fc6d
nnnsss12 <ms.nora1424@gmail.com>
2026-06-01 13:04:30 +0000

    Fix 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # Create a new sentence
 # Using string slicing
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
 print(str[39:66] + str[106:112] + str[:6])
