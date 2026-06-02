2bf33f79ef4a6a80845c9f862374131d0d4aaf8a
nnnsss12 <ms,nora1424@gmail.com>
2026-06-02 12:04:26 +0000

    Fix uppercase to use only 2 prints

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
+    result = ""
     for c in str:
         if ord('a') <= ord(c) <= ord('z'):
-            print("{}".format(chr(ord(c) - 32)), end="")
+            result += "{}".format(chr(ord(c) - 32))
         else:
-            print("{}".format(c), end="")
-    print("")
+            result += "{}".format(c)
+    print("{}".format(result))
