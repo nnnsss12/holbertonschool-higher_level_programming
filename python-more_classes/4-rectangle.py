7edad607eeca4f4beea7fecdc61536a76b88b9ef
nnnsss12 <nnnsss12@gmail.com>
2026-06-18 17:59:09 +0000

    Add python-more_classes task 4 - Rectangle with eval-compatible repr

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""Module that defines a Rectangle class with eval-compatible repr."""
+
+
+class Rectangle:
+    """A class that defines a rectangle with repr for eval recreation."""
+
+    def __init__(self, width=0, height=0):
+        """Instantiates a Rectangle with optional width and height."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieves the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the width with validation."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieves the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the height with validation."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Returns the rectangle area."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Returns the rectangle perimeter, or 0 if width or height is 0."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Returns string representation of rectangle using # characters."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return "\n".join("#" * self.__width for i in range(self.__height))
+
+    def __repr__(self):
+        """Returns official string representation to recreate instance."""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
