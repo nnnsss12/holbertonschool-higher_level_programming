d4dd463b8ee7c89aa7b9b830f4020d205808667f
nnnsss12 <nnnsss12@gmail.com>
2026-06-18 17:38:36 +0000

    Add python-more_classes task 1 - Rectangle with width and height

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Module that defines a Rectangle class with width and height."""
+
+
+class Rectangle:
+    """A class that defines a rectangle with validated width and height."""
+
+    def __init__(self, width=0, height=0):
+        """Instantiates a Rectangle with optional width and height."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieves the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the width with validation."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieves the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the height with validation."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
