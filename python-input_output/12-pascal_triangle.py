3efcb8ddc5f2a8e3c8f43705558d87203264040d
nnnsss12 <ms.nora1424@gmail.com>
2026-07-01 13:32:12 +0000

    Add task 12 - Pascal's Triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module that defines a Pascal's Triangle function."""
+
+
+def pascal_triangle(n):
+    """Return a list of lists of integers representing Pascal's triangle of n."""
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    while len(triangle) < n:
+        prev_row = triangle[-1]
+        # تبدأ الصف الجديد دائماً بـ 1، وتجمع كل عنصرين متجاورين من الصف السابق، وتنتهي بـ 1
+        new_row = [1]
+        for i in range(len(prev_row) - 1):
+            new_row.append(prev_row[i] + prev_row[i + 1])
+        new_row.append(1)
+        triangle.append(new_row)
+
+    return triangle
