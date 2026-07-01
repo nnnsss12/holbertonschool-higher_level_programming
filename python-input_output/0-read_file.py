c844132370ce9a7ac6e6ff65a5fc887cd1af0eca
nnnsss12 <ms.nora1424@gmail.com>
2026-07-01 13:08:45 +0000

    Fix task 0 - add docstrings

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Module for reading a file and printing its content."""
+
+
 def read_file(filename=""):
+    """Read a text file and print its content to stdout."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
