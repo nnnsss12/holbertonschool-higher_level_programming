ab1c4d4a7d300934a4a5b198aed5fecd3bbb03c7
nnnsss12 <ms.nora1424@gmail.com>
2026-07-21 13:23:30 +0000

    Add task 15: number by score

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,2 @@
+-- lists the number of records for each score in second_table, ordered by number (descending)
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
