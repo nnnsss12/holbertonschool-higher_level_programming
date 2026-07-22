3ea327682deb5a30a5df63c37e74453c98930b43
nnnsss12 <ms.nora1424@gmail.com>
2026-07-22 13:18:08 +0000

    Use BINARY for case-sensitive N filter

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,8 @@ if __name__ == "__main__":
         host="localhost", port=3306,
         user=username, passwd=password, db=db_name)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute(
+        "SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
