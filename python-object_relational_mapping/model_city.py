b7bdfbb161aaec113144f6a50bf32ff2b39d3419
Noura sultan <ms.nora1424@gmail.com>
2026-07-22 16:52:25 +0300

    Refactor City class and update docstrings

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""Defines the City class linked to the MySQL table cities."""
-from sqlalchemy import Column, Integer, String, ForeignKey
+"""Module that defines the City class, linked to the MySQL cities table."""
 from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(Base):
-    """Represents a city in the cities table."""
+    """Represent a city, mapped to the MySQL cities table."""
+
     __tablename__ = "cities"
-    id = Column(Integer, primary_key=True, nullable=False,
-                autoincrement=True)
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    state = relationship("State", backref="cities")
